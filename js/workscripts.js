fb0a20c3ed02c70d3b4eff5445624b7c54b8e7fb
Devesh Kumar <devesh2027@gmail.com>
2019-05-07 20:29:24 +0530

    Added more pinned repos.

--- js/workscripts.js
+++ js/workscripts.js
@@ -1,8 +1,6 @@
 function opennav()
 {
-	{
-		document.getElementById('sidenav').style.width='100%';
-	}
+	document.getElementById('sidenav').style.width='100%';
 }
 
 function closenav()
@@ -11,7 +9,7 @@ function closenav()
 }
 
 function blessco(){
-	var blessarray=[
+	let blessarray=[
 		"<i class='far fa-heart fa-lg'></i>",
 		"<i class='fas fa-heart fa-lg'></i>",
 		"<i class='fas fa-coffee fa-lg'></i>",
@@ -24,12 +22,55 @@ function blessco(){
 		"<i class='fas fa-apple-alt fa-lg'></i>"
 	];
 
-	var randomnum=Math.floor(Math.random()*10);   // Random number.
+	let randomnum=Math.floor(Math.random()*10);   // Random number.
 
-	for(var i=0;i<document.getElementsByClassName('blesscont').length;i++)
+	for(let i=0;i<document.getElementsByClassName('blesscont').length;i++)
 	{
 		document.getElementsByClassName('blesscont')[i].innerHTML=blessarray[randomnum];
 	}
 }
 
 window.addEventListener('load',blessco);
+
+// JS Object containing all the pinned repos.
+
+let otherPinnedRepos = {
+	Pollster : {
+		url: "https://github.com/deve-sh/Pollster",
+		description: "A Sexy Voting Web App!",
+		language:"PHP"
+	},
+	"Regression-Analysis" : {
+		url : "https://github.com/deve-sh/Regression-Analysis",
+		description : "📈 A Python Library for Regression Analysis. Both Linear and Non - Linear.",
+		language : "Python"
+	},
+	learnpython :{
+		url : "https://github.com/deve-sh/learnpython",
+		description : "🐍 A Website to learn Python basics as fast as possible!",
+		language : "HTML"
+	}
+}
+
+function repoFiller(){
+	// Function to populate repos in work.html.
+
+	let pinnedHTML = ``;
+
+	for(let repo in otherPinnedRepos){
+		if(otherPinnedRepos.hasOwnProperty(repo)){
+			pinnedHTML += `<div class='repo'>
+				<a href='${otherPinnedRepos[repo]["url"]}' target='_blank'>
+					<div class='reponame'>
+						${repo}
+					</div>
+				</a>
+				<div class='repodesc'>
+					${otherPinnedRepos[repo]["description"]}
+				</div>
+			</div>`;
+		}
+	}
+
+	document.getElementById('repofiller').innerHTML = pinnedHTML;
+}
