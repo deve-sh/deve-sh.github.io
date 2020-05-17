ddc73a586a8d2d4ae2100ded0d33099432d28f57
Devesh Kumar <devesh2027@gmail.com>
2020-05-17 13:16:23 +0530

    Started with contact page and added two sections.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,21 +2,115 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Header from "../components/header"
-import Footer from "../components/footer";
+import Footer from "../components/footer"
 import Image from "../components/image"
+import Icon from "../components/icon"
 import SEO from "../components/seo"
+
+import ContactImage from "../images/conversation.svg"
 
 const ContactPage = () => {
 	return (
 		<React.Fragment>
-		    <SEO title="Devesh Kumar - Contact" />
-		    <Header />
-		    <div className={"contact"}>
+			<SEO title="Devesh Kumar - Contact" />
+			<Header />
+			<div className={"contact"}>
+				<section className={"contact-toprow"}>
+					<div className={"fixedcontainer row invertrow"}>
+						<div className={"col-md-6 contactdesc"}>
+							<div className={"contact-toprow-heading"}>Contact Me</div>
+							<div className={"contact-toprow-desc"}>
+								<p>
+									Have an Idea? Wanna work together? Or Just want to say Hi?
+									Feel free to get in contact with me.
+								</p>
 
-		    </div>
-		    <Footer />
+								<p>I am always looking for the next opportunity.</p>
+
+								<p>
+									Find my socials and a form to contact me in the section below.
+									Great to have you here!
+								</p>
+							</div>
+						</div>
+						<div className={"col-md-6 contactimage"}>
+							<Image src={ContactImage} alt={"Contact Me"} />
+						</div>
+					</div>
+				</section>
+				<div className={"contact-separators"}>
+					<div className={"dot"} />
+					<div className={"dot"} />
+					<div className={"dot"} />
+				</div>
+				<section className={"contact-socialsection"}>
+					<a
+						href={"https://github.com/deve-sh/"}
+						className={"sociallink"}
+						target={"_blank"}
+						rel={"noopener noreferrer"}
+						title={"View My Github Profile"}
+						aria-label={"View My Github Profile"}
+					>
+						<Icon
+							className={"fab fa-github fa-lg"}
+							title={"View My Github Profile"}
+						/>
+					</a>
+					<a
+						href={"https://www.linkedin.com/in/dev-esh/"}
+						className={"sociallink linkedin"}
+						target={"_blank"}
+						rel={"noopener noreferrer"}
+						title={"View My LinkedIn Profile"}
+						aria-label={"View My LinkedIn Profile"}
+					>
+						<Icon
+							className={"fab fa-linkedin-in fa-lg"}
+							title={"View My LinkedIn Profile"}
+						/>
+					</a>
+					<a
+						href={"https://instagram.com/d_e_v.e_s_h"}
+						className={"sociallink instagram"}
+						target={"_blank"}
+						rel={"noopener noreferrer"}
+						title={"View My Instagram Profile"}
+						aria-label={"View My Instagram Profile"}
+					>
+						<Icon
+							className={"fab fa-instagram fa-lg"}
+							title={"View My Instagram Profile"}
+						/>
+					</a>
+					<a
+						href={"mailto:devesh2027@gmail.com"}
+						className={"sociallink mail"}
+						title={"Email Me"}
+						aria-label={"Email Me"}
+					>
+						<Icon
+							className={"fas fa-envelope fa-lg"}
+							title={"Email Me"}
+						/>
+					</a>
+				</section>
+				<section className={"contact-form-container"}>
+					<div className={"fixedcontainer"}>
+						<form
+							className={"contact-form"}
+							target={"_blank"}
+							rel={"noopener noreferrer"}
+							method={"POST"}
+						>
+							<div className={"contact-form-heading"}>Contact me</div>
+						</form>
+					</div>
+				</section>
+			</div>
+			<Footer />
 		</React.Fragment>
-	);
-};
+	)
+}
 
-export default ContactPage;
+export default ContactPage
