2dbb87f2543b17c7e75296a6d3f91e6e05ef1c97
Devesh Kumar <devesh2027@gmail.com>
2023-08-18 08:02:05 +0530

    feat: Resume content and styling  update

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -39,7 +39,7 @@ const ResumePage = () => {
         <a
           href={link}
           className="experience-snippet-link"
-          rel="noopener norferrer"
+          rel="noopener noreferrer"
           target="_blank"
           title={title || company || desc}
           aria-label={title || company || desc}
@@ -50,7 +50,14 @@ const ResumePage = () => {
       {duration && (
         <div className="experience-snippet-duration">{duration}</div>
       )}
-      {desc && <div className="experience-snippet-desc">{desc}</div>}
+      {desc && (
+        <div
+          className="experience-snippet-desc"
+          style={{ whiteSpace: "pre-wrap", lineHeight: 2 }}
+        >
+          {desc}
+        </div>
+      )}
     </div>
   )
 
@@ -149,23 +156,27 @@ const ResumePage = () => {
             <SectionHeading>Skills</SectionHeading>
             <SkillSectionClass
               type="Languages"
-              skillset="JavaScript, Python, C, C++, Java, SQL, HTML, CSS, PHP"
+              skillset="JavaScript, TypeScript, Python, C, C++, SQL, HTML, CSS, PHP"
             />
             <SkillSectionClass
               type="Frameworks"
-              skillset="React, Next.js, Gatsby, Node.js, Express, Svelte, Vue, Jest, Enzyme, Flask, GraphQL"
+              skillset="React, Next.js, Gatsby, Node.js, Express, Svelte, Vue, Tailwind, SASS, GraphQL"
             />
             <SkillSectionClass
               type="Databases"
-              skillset="Firebase, MongoDB, PostgreSQL, MySQL, PlanetScale"
+              skillset="Firestore, MongoDB, PostgreSQL, MySQL"
+            />
+            <SkillSectionClass
+              type="Testing"
+              skillset="Jest, Enzyme, React Testing Library, Cypress, Playwright"
+            />
+            <SkillSectionClass
+              type="Platforms"
+              skillset="Firebase, Supabase, GCP, AWS, Hasura"
             />
             <SkillSectionClass
               type="Tools"
-              skillset="Git, Agile Development, Scrum"
-            />
-            <SkillSectionClass
-              type="Soft Skills"
-              skillset="Excellent Communicator, Responsible, Owner, Passionate"
+              skillset="Git, Turbo, Nx, Vite"
             />
           </section>
           <div className="row resume-additioninfo">
@@ -175,65 +186,77 @@ const ResumePage = () => {
               <ExperienceSnippet
                 hasDot
                 title="Senior Software Engineer"
+                company="Postman"
+                duration="Feb 2023 - Present"
+                desc={`Part of the core Web Platform team.
+- Working on Modular Application Architectures using Micro-Frontends to deliver Postman on any platform.
+- Worked extensively on the development and shipping of the Postman VS Code Extension. Assessed, migrated and shipped end-to-end features such as GraphQL and gRPC Workbench.`}
+              />
+              <ExperienceSnippet
+                hasDot
+                title="Senior Software Engineer"
                 company="Unacademy"
-                duration="Apr 2022 - Present"
-                desc=""
+                duration="Apr 2022 - Feb 2023"
+                desc={`- Reduced Average Page Size by 30% for faster load.
+- Migrated Common Redux store to SWR and Zustand to save each user over 500KB/visit.
+- Implemented Pipeline parallelization and build caching to reduce integration and deployment pipeline time by 10-15 minutes/build.
+- Worked on the creation of a new standardized Design System for the Web Engineering organization.
+- Worked on POCs for splitting frontend Next.js Monolith into Micro-Frontends.`}
               />
               <ExperienceSnippet
                 hasDot
                 title="Software Engineer"
                 company="Unacademy"
                 duration="Jul 2021 - Apr 2022"
-                desc={`Highest contributor since joining. 
-                Part of platform team. 
-                Optimized site, developed POCs in new technologies, 
-                documented and assisted other teams with features. 
-                Conducted Interviews and assisted in hiring of candidates.`}
+                desc={`Part of the Web Platform Team.
+- Focused on seamless and minimal interruption platform-level changes + Keeping the codebase clean and performant as the product evolves and scaled to 4x the number of developers.
+- Conducted over 40 technical interviews and participated in hiring processes.
+- Worked with teams to create and roll out new projects like Unacademy Icons, Global Test Prep, and internal CRM Setup, and their integration into the monolith.
+- Spearheaded the migration of universally server-rendered monolith to opt-in static optimization, reducing average page load time by 45% and saving compute and bandwidth costs.
+- Worked on Technical and Product Documentation for efficient organisation-wide knowledge transfers.
+- Highest Contributor to Codebase since joining.`}
               />
               <ExperienceSnippet
                 hasDot
                 title="Lead Engineer"
                 company="Unergia & Solar Ladder"
                 duration="Apr 2020 - Jul 2021"
-                desc={`Led the work on architecting, creation and scaling of
-                Unergia's SAAS Platform: SolarLadder, their real-time
-                Business bidding platform, Blog Engine and Residential
-                Solar Marketplace from scratch. Used A/B tests to
-                increase website conversion rate by 300%.`}
+                desc={`Single-handedly built SolarLadder, a B2B SAAS offering for Solar Companies, from scratch.
+SolarLadder provides over 30 different features to solar companies so they can focus on serving their customers better, reduce costs and automate every aspect of their operations.
+Most tasks that took over an hour manually, take less than a minute to complete on SolarLadder.
+
+Crafted Unergia's proprietary B2C Platform end to end from concept to execution to connect Solar Installers focusing on the Residential sector to potential customers with Push Notifications for User Engagement and Payment Gateway integration.
+Performed A/B and Redirect Tests on Unergia's main website, optimized their website and decreased bounce rate by 25%, latency by 85% and increased conversion rate by 200%.
+Created an in-house blogging system for Unergia using Next.js, Redux, Bootstrap and Firebase and worked with SEO best practices for better reach and targeting.
+Integrated Unergia's CRM with their customers to automate geographical tagging processes. Migrated Unergia's main website to SEO Optimized and Static Next.js pages.`}
               />
               <ExperienceSnippet
                 hasDot
                 title="Full Stack Web Developer"
                 company="Maalexi"
                 duration="Apr 2020 - Jul 2021"
-                desc={`Worked on the backend and frontend of a next-gen
-                online marketplace using Next.js, Express, MongoDB
-                (Atlas) and AWS Cloud Offerings.`}
+                desc={`Worked on the backend and frontend of a next-gen online marketplace using Next.js, Express, MongoDB (Atlas) and AWS Cloud Offerings.`}
               />
               <ExperienceSnippet
                 hasDot
                 title="Backend Engineer"
                 company="EventBeep"
                 duration="Dec 2020 - Feb 2021"
-                desc={`Worked on efficient delivery of notifications to users
-                and the GraphQL backend for the mobile app.`}
+                desc={`Worked on efficient delivery of notifications to users and the GraphQL backend for the mobile app.`}
               />
               <ExperienceSnippet
                 hasDot
                 title="Web Development Intern"
                 company="JagoInvestor.com"
                 duration="Jan 2020 - Feb 2020"
-                desc={`Developed Financial Tools and Calculators for
-                JagoInvestor's website, setup data warehousing using
-                Firestore and CRM Integration to generate more leads.`}
+                desc={`Developed Financial Tools and Calculators for JagoInvestor's website, setup data warehousing using Firestore and CRM Integration to generate more leads.`}
               />
               <ExperienceSnippet
                 hasDot
                 title="React.js Development Intern"
                 company="Convin.ai"
                 duration="Sep 2019 - Nov 2019"
-                desc={`Worked on developing efficient and beautiful Dashboard modules and components using React.JS and Redux for Convin's ML Platform, 
-                with Code Obfuscation for increased Code Security.`}
+                desc={`Worked on developing efficient and beautiful Dashboard modules and components using React.JS and Redux for Convin's ML Platform, with Code Obfuscation for increased Code Security.`}
               />
             </section>
             <section className="col-md-5 resume-additionalinfo-certifications">
@@ -268,27 +291,27 @@ const ResumePage = () => {
               <br />
               <SectionHeading>Projects</SectionHeading>
               <ExperienceSnippet
-                title="Hobnob"
-                company="An efficient Code-Casting service on the web
-                to help instructors move their courses online."
-                link="https://devesh.tech/hobnob"
-                duration="Nov 2019 - Apr 2020"
+                title="Tranquil"
+                company="A real time online development environment to
+                simplify project bootstrapping and testing."
+                link="https://github.com/deve-sh/Tranquil"
+                duration="Dec 2022 - Feb 2023"
               />
               <div className="row">
                 <div className="col-6 p-1 pl-0">
                   <ExperienceSnippet
-                    title="Smallet"
-                    company="A Simplet Digital Wallet"
-                    link="https://smallet.now.sh"
-                    duration="Oct 2021 - Nov 2021"
+                    title="Isomorph"
+                    company="A Framework to build Isomorphic React apps. Inspired by Next.js."
+                    link="https://npmjs.com/isomorph-web"
+                    duration="May 2022 - Jun 2022"
                   />
                 </div>
                 <div className="col-6 pl-1">
                   <ExperienceSnippet
-                    title="NoteItDown"
-                    company="A Notion-Like workspace for teams."
-                    link="https://noteitdown.now.sh"
-                    duration="Sep 2021 - Oct 2021"
+                    title="Korero"
+                    company="A simple snippet that enables real-time communication for your team on any page"
+                    link="https://github.com/deve-sh/korero"
+                    duration="Feb 2022 - Mar 2022"
                   />
                 </div>
               </div>
